dce3252d0e50f4443bb0f2cf4340c763aea5bca8
CrazyBanana333 <tonellibradyn@gmail.com>
2024-01-10 17:17:51 -0500

    added randomized status messages

--- src/discordBot.js
+++ src/discordBot.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const getVideo = require('./YoutubeRequest.js');
 const streamRequest = require('./TwitchRequest.js');
 
+var jsonData = JSON.parse(fs.readFileSync('./program_data.json', {encoding: 'utf-8', flag: 'r'}));
+
 var mostRecentUploadDate = new Date(0);
 
 const { Client, IntentsBitField, ActivityType} = require('discord.js');
@@ -33,6 +35,7 @@ function pollVideo(data){
         mostRecentUploadDate = publishDate;
         client.channels.fetch(process.env.YOUTUBECHANNELID)
         .then(channel => {
+
             channel.send('<@&1190910279236735067>\n 🎉 NEW VIDEO GO WATCH IT RIGHT NOW 🎉\nhttps://www.youtube.com/watch?v=' + data.id.videoId);
         })
     }
@@ -45,14 +48,7 @@ function pollLive(data){
         .then(channel => {
             channel.send('bro went offline im sobbing :((((');
         })
-        client.user.setPresence({
-            activities: [{
-                name: "the sound of my own tears :(",
-                url: "https://www.twitch.tv/shyjinn",
-                state: "crying and sobbing rn",
-                type: ActivityType.Listening
-            }]
-        })
+
         jsonData.isLive = false;
         fs.writeFileSync('./program_data.json', JSON.stringify(jsonData));
 
@@ -63,44 +59,151 @@ function pollLive(data){
         .then(channel => {
             channel.send('<@&1190402493260251256> HE JUST WENT LIVE GO WATCH HIM AT https://www.twitch.tv/shyjinn RIGHT NOW\n' + data[0].title);
         })
-            client.user.setPresence({
-        activities: [{
-            name: "ShyJinn",
-            url: "https://www.twitch.tv/shyjinn",
-            state: "at https://www.twitch.tv/shyjinn right now",
-            type: ActivityType.Watching
-        }]
-    })
+
         jsonData.isLive = true;
         fs.writeFileSync('./program_data.json', JSON.stringify(jsonData));
         console.log('Shyjinn is now live');
         return;
     }
+
+    if (jsonData.isLive == false) {
+        newStatus = Math.floor(Math.random() * 4);
+        switch (newStatus){
+            case 0:
+                client.user.setPresence({
+                    activities: [{
+                        name: "the sound of my own tears :(",
+                        url: "https://www.twitch.tv/shyjinn",
+                        state: "crying and sobbing rn",
+                        type: ActivityType.Listening
+                    }]
+                });
+                break;
+            case 1:
+                client.user.setPresence({
+                    activities: [{
+                        name: "a sadness contest",
+                        url: "https://www.twitch.tv/shyjinn",
+                        state: "because my favorite streamer is offline :(",
+                        type: ActivityType.Competing
+                    }]
+                });
+                break;
+            case 2:
+                client.user.setPresence({
+                    activities: [{
+                        name: "shyjinns offline chat waiting",
+                        url: "https://www.twitch.tv/shyjinn",
+                        state: "he'll be here any second I know it",
+                        type: ActivityType.Watching
+                    }]
+                });
+                break;
+            case 3:
+                client.user.setPresence({
+                    activities: [{
+                        name: "waiting",
+                        url: "https://www.twitch.tv/shyjinn",
+                        state: "Waiting for shyjinn at a medium pace",
+                        type: ActivityType.Custom
+                    }]
+                });
+                break;
+        }
+    } else {
+        newStatus = Math.floor(Math.random() * 3);
+        switch (newStatus){
+            case 0:
+                client.user.setPresence({
+                    activities: [{
+                        name: "ShyJinn",
+                        url: "https://www.twitch.tv/shyjinn",
+                        state: "at https://www.twitch.tv/shyjinn right now",
+                        type: ActivityType.Watching
+                    }]
+                });
+                break;
+            case 1:
+                client.user.setPresence({
+                    activities: [{
+                        name: "HES STREAMING",
+                        url: "https://www.twitch.tv/shyjinn",
+                        state: "HES STREAMING! MY FAVORITE STREAMER SHYJINN",
+                        type: ActivityType.Custom
+                    }]
+                });
+                break;
+            case 2:
+                client.user.setPresence({
+                    activities: [{
+                        name: "in a gift sub competition",
+                        url: "https://www.twitch.tv/shyjinn",
+                        state: "i will gift the most",
+                        type: ActivityType.Competing
+                    }]
+                });
+                break;
+        }
+    }
 }
 
-//DISCORD STUFF
+//YOUTUBE AND TWITCH POLLING
 client.on('ready', (c) => {
     console.log(`${c.user.tag} is logged in`);
 //MAKE SURE YOU WONT MAX OUT YOUTUBE API BEFORE UNCOMMENTING THESE
-    // getVideo(process.env.APIKEY, initializeDate);
-    // setInterval(function(){
-    //      streamRequest(process.env.AUTHCODE, process.env.CLIENTID, pollLive)
+    getVideo(process.env.APIKEY, initializeDate);
+    setInterval(function(){
+         streamRequest(process.env.AUTHCODE, process.env.CLIENTID, pollLive)
+    }, 120000)
+    setInterval(function(){
+        getVideo(process.env.APIKEY, pollVideo)
+    }, 900000)
 
-    // }, 120000)
-    // setInterval(function(){
-    //     getVideo(process.env.APIKEY, pollVideo)
-    // }, 900000)
-
-    client.user.setPresence({
-        activities: [{
-            name: "the sound of my own tears :(",
-            url: "https://www.twitch.tv/shyjinn",
-            state: "crying and sobbing rn",
-            type: ActivityType.Listening
-        }]
-    })
+    newStatus = Math.floor(Math.random() * 4);
+        switch (newStatus){
+            case 0:
+                client.user.setPresence({
+                    activities: [{
+                        name: "the sound of my own tears :(",
+                        url: "https://www.twitch.tv/shyjinn",
+                        state: "crying and sobbing rn",
+                        type: ActivityType.Listening
+                    }]
+                });
+                break;
+            case 1:
+                client.user.setPresence({
+                    activities: [{
+                        name: "a sadness contest",
+                        url: "https://www.twitch.tv/shyjinn",
+                        state: "because my favorite streamer is offline :(",
+                        type: ActivityType.Competing
+                    }]
+                });
+                break;
+            case 2:
+                client.user.setPresence({
+                    activities: [{
+                        name: "shyjinns offline chat waiting",
+                        url: "https://www.twitch.tv/shyjinn",
+                        state: "he'll be here any second I know it",
+                        type: ActivityType.Watching
+                    }]
+                });
+                break;
+            case 3:
+                client.user.setPresence({
+                    activities: [{
+                        name: "waiting",
+                        url: "https://www.twitch.tv/shyjinn",
+                        state: "Waiting for shyjinn at a medium pace",
+                        type: ActivityType.Custom
+                    }]
+                });
+                break;
+        }
         
-})
+});
 
 client.on('interactionCreate', (interaction) => {
     if (!interaction.isChatInputCommand()) return;
@@ -141,17 +244,6 @@ client.on('messageCreate', (message) => {
             //message.reply(mostRecentUploadDate.toString());
             break;
     }
-    function ifLive(data){
-        if (data.length === 0){
-            message.reply("my favorite streamer shyjinn is not live currently :(((")
-            return false;
-        } else {
-            message.reply("HES LIVE GO WATCH HIM AT https://www.twitch.tv/shyjinn RIGHT NOW")
-            return true;
-        }
-    }
-
-
 }
 );
 
