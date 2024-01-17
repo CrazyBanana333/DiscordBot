f7725ed483ffe9d03a31e3ada08acfa5445a24ec
CrazyBanana333 <tonellibradyn@gmail.com>
2024-01-16 20:46:22 -0500

    reverted some changes

--- scripts/startTwitchBot.js
+++ scripts/startTwitchBot.js
@@ -3,3 +3,8 @@ const refreshToken = require('./../src/tokenRefresh.js');
 
 //START TWITCH BOT
 refreshToken(startTwitchBot);
+
+//restart every 4 hours
+ setInterval(() => {
+     refreshToken(startTwitchBot);
+ }, (4*60*60*1000));
